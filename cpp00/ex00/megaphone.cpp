dc9fe7f76ea95c11494cc27e099af7c79b63edbf
Simow03 <imthetigerz@gmail.com>
2024-11-26 05:50:58 +0100

    solve_ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+std::string toUpperStr(std::string s)
+{
+    for (int i = 0; i < (int)s.length(); i++)
+        s[i] = toupper(s[i]);
+    return s;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc >= 2)
+    {
+        for (int i = 1; i < argc; i++)
+            std::cout << toUpperStr(argv[i]);
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << "\n";
+}
