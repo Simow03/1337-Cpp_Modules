2f13279ac047b2328ded5ec418446916e11849c4
Simow03 <imthetigerz@gmail.com>
2024-12-17 01:17:15 +0100

    reformulate_main

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -9,21 +9,25 @@ Harl::Harl() {
 
 void Harl::debug(void)
 {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void)
 {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -38,5 +42,5 @@ void Harl::complain(std::string level)
         }
     }
 
-    std::cout << "WRONG COMPLAINT !" << std::endl;
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -3,27 +3,22 @@
 int main() {
     Harl harl;
 
-    std::cout << "[DEBUG]" << std::endl;
     harl.complain("DEBUG");
     std::cout << std::endl;
 
-    std::cout << "[INFO]" << std::endl;
+
     harl.complain("INFO");
     std::cout << std::endl;
 
-    std::cout << "[WARNING]" << std::endl;
     harl.complain("WARNING");
     std::cout << std::endl;
 
-    std::cout << "[ERROR]" << std::endl;
     harl.complain("ERROR");
     std::cout << std::endl;
 
-    std::cout << "[WRONG]" << std::endl;
     harl.complain("WRONG");
     std::cout << std::endl;
 
-    std::cout << "[]" << std::endl;
     harl.complain("");
     std::cout << std::endl;
 
