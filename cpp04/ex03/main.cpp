b73401161c11c72515b0af6701a18297284bbe4c
Simow03 <imthetigerz@gmail.com>
2025-01-16 18:55:58 +0100

    turn_test_cases

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -4,14 +4,8 @@
 #include "Cure.hpp"
 #include "MateriaSource.hpp"
 
-void f()
-{
-    system("leaks interfaces");
-}
-
 int main()
 {
-    // atexit(f);
     IMateriaSource *src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
@@ -24,8 +18,30 @@ int main()
     ICharacter *bob = new Character("bob");
     me->use(0, *bob);
     me->use(1, *bob);
+
+    Character *original = new Character("original");
+    original->equip(src->createMateria("ice"));
+    Character *copy = new Character(*original);
+    delete original;
+    std::cout << "\n - - - copy ptr use() : - - - " << std::endl;
+    copy->use(0, *bob);
+
+    Character *full = new Character("full");
+    for (int i = 0; i < 4; i++)
+        full->equip(src->createMateria("ice"));
+
+    full->unequip(0);
+    full->equip(src->createMateria("cure"));
+
+
+    std::cout << "\n - - - full ptr use() : - - - " << std::endl;
+    full->use(0, *bob);
+
+    delete copy;
+    delete full;
     delete bob;
     delete me;
     delete src;
+
     return 0;
 }
