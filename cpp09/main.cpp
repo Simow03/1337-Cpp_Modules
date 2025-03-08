53df7e9575d9d4afc859e71ec49e185b4a2355cc
Mohamed Staali <imthetigerz@gmail.com>
2025-03-08 01:31:38 +0000

    split_content & parse_values

---
+++ cpp09/main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include "splitString.cpp"
+
+bool isValidFileName(std::string filename) {
+    return filename == "input.txt" ? true : false;
+}
+
+std::string readFromFile() {
+    std::ifstream inputFile("input.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: could not open file." << std::endl;
+        exit(1);
+    }
+    std::stringstream fileContentStream;
+
+    fileContentStream << inputFile.rdbuf();
+
+    return fileContentStream.str();
+}
+
+bool validateDate(std::string& date) {
+    if (date.length() != 11 || date[4] != '-' || date[7] != '-')
+        return false;
+    
+    std::string yearStr = date.substr(0, 4);
+    std::string monthStr = date.substr(5, 2);
+    std::string dayStr = date.substr(8, 2);
+
+    for (size_t i = 0; i < yearStr.length(); i++)
+        if (!isdigit(yearStr[i]))
+            return false;
+    for (size_t i = 0; i < monthStr.length(); i++)
+        if (!isdigit(monthStr[i]))
+            return false;
+    for (size_t i = 0; i < dayStr.length(); i++)
+        if (!isdigit(dayStr[i]))
+            return false;
+    
+    int year = atoi(yearStr.c_str());
+    int month = atoi(monthStr.c_str());
+    int day = atoi(dayStr.c_str());
+
+    if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
+        return false;
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        if (day > 30)
+            return false;
+    }
+    else if (month == 2) {
+        bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        if (day > (isLeapYear ? 29 : 28))
+            return false;
+    }
+
+    return true;
+}
+
+bool validateValue(std::string& value) {
+
+    int startPos = (value[1] == '-' ? 2 : 1);
+
+    if (startPos != 1) {
+        std::cout << "Error: not a positive number." << std::endl;
+        return false;
+    }
+
+    bool isNumeric = true;
+    bool isFloat = false;
+
+    for (size_t i = startPos; i < value.length(); i++) {
+        if (value[i] == '.' && !isFloat)
+            isFloat = true;
+        else if (!isdigit(value[i])) {
+            isNumeric = false;
+            break;
+        }
+    }
+    if (!isNumeric)
+        return false;
+
+    double doubleValue = std::strtod(value.c_str(), NULL);
+
+    if (doubleValue > 1000) {
+        std::cout << "Error: too large a number." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void parseInputFile() {
+
+    std::string fileContent = readFromFile();
+
+    if(fileContent.empty()) {
+        std::cerr << "Error: file is empty." << std::endl;
+        exit(1);
+    }
+
+    std::string* contentArray = splitString(fileContent, '\n');
+
+    if (contentArray[1] != "date | value") {
+        std::cerr << "Error: invalid header format." << std::endl;
+        freeSplitString(contentArray);
+        exit(1);
+    }
+
+    size_t count = atoi(contentArray[0].c_str());
+    for (size_t i = 2; i <= count; i++) {
+        std::string* dateValueArray = splitString(contentArray[i], '|');
+        size_t tokens = atoi(dateValueArray[0].c_str());
+        if (tokens != 2) {
+            std::cout << "Error: bad input => " << contentArray[i] << std::endl;
+            freeSplitString(dateValueArray);
+            continue;
+        }
+        if (!validateDate(dateValueArray[1])) {
+            std::cout << "Error: bad date format => " << dateValueArray[1] << std::endl;
+            freeSplitString(dateValueArray);
+            continue;
+        }
+        if (!validateValue(dateValueArray[2])) {
+            freeSplitString(dateValueArray);
+            continue;
+        }
+        std::cout << contentArray[i] << std::endl;
+        freeSplitString(dateValueArray);
+    }
+}
+
+int main(int ac, char **av) {
+    if (ac != 2) {
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    if (!isValidFileName(av[1])) {
+        std::cerr << "Error: filename does not match." << std::endl;
+        return 1;
+    }
+    parseInputFile();
+}
---
+++ cpp09/splitString.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <iostream>
+
+int countSegments(const std::string& str, char delimiter) {
+    int count = 0;
+
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str.find(delimiter, start)) != std::string::npos) {
+        if(end != start)
+            count++;
+        
+        start = end + 1;
+    }
+
+    if (start < str.length())
+        count++;
+    
+    return count;
+}
+
+std::string* splitString(std::string fileContent, char delimiter) {
+    int segmentCount = countSegments(fileContent, delimiter);
+
+
+    std::string* result = new std::string[segmentCount + 1];
+    
+    char countStr[20];
+    sprintf(countStr, "%d", segmentCount);
+    result[0] = countStr;
+
+    size_t i = 1;
+    size_t start = 0;
+    size_t end = 0;
+
+    while((end = fileContent.find(delimiter, start)) != std::string::npos) {
+        if (end != start)
+            result[i++] = fileContent.substr(start, end - start);
+
+        start = end + 1;
+    }
+
+    if (start < fileContent.length())
+        result[i] = fileContent.substr(start);
+
+    return result;
+}
+
+void freeSplitString(std::string* array) {
+    if (array != NULL)
+        delete[] array;
+}
